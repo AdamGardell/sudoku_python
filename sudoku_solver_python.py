7e10bc3cce8f6b4f8e584718d9a3f4c0cbc650f6
Adam Gardell <adam.gardell@outlook.com>
2020-04-04 23:30:54 -0400

    implement basic generate_board function. Create solve_game function

--- sudoku_solver_python.py
+++ sudoku_solver_python.py
@@ -7,3 +7,33 @@ Created on Sat Apr  4 21:51:42 2020
 
 #Sudoku Solver!
 
+# Return a sudoku game board
+# In the future change this to generate a board randomly
+# for now put in a board manually
+# Board taked from websudoku.com
+def generate_board():
+    board = ([0, 8, 0, 6, 0, 0, 0, 0, 7],
+             [0, 0, 5, 0, 9, 2, 6, 0, 0],
+             [0, 3, 0, 4, 0, 0, 0, 5, 9],
+             [0, 9, 0, 8, 6, 0, 4, 0, 0],
+             [8, 2, 0, 5, 0, 9, 0, 1, 6],
+             [0, 0, 6, 0, 2, 7, 0, 9, 0],
+             [9, 6, 0, 0, 0, 4, 0, 7, 0],
+             [0, 0, 2, 9, 1, 0, 5, 0, 0],
+             [1, 0, 0, 0, 0, 3, 0, 6, 0])
+    
+    return board
+
+
+# input a new game board, output the solved board
+def solve_game(board):
+    
+    
+    
+    
+    
+    return solved_board
+    
+
+    
+    
